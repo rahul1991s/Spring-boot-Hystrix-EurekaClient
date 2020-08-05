1a0d5d1a2673e24c81cae17b0f354bf608a7b270
Encora_Rahul S <rahul191291@gmail.com>
2020-08-05 18:45:34 +0800

    WebFlux dependency

--- movie-info-service/src/main/java/com/rahuls/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/com/rahuls/movieinfoservice/resources/MovieResource.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.rahuls.movieinfoservice.models.Movie;
@@ -25,6 +26,9 @@ public class MovieResource {
 	@Autowired
 	private RestTemplate restTemplate;
 
+    @Autowired
+    WebClient.Builder webClientBuilder;
+
 	@HystrixCommand(fallbackMethod = "getFallBackMovieInfo")
 	@RequestMapping("/{movieId}")
 	public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
@@ -34,6 +38,12 @@ public class MovieResource {
 		return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
 	}
 
+	/*
+	Alternative WebClient way
+	Movie movie = webClientBuilder.build().get().uri("http://localhost:8082/movies/"+ rating.getMovieId())
+	.retrieve().bodyToMono(Movie.class).block();
+	*/
+	
 	public Movie getFallBackMovieInfo(@PathVariable("movieId") String movieId) {
 		return new Movie("NA", "No Movie", "Service is Down");
 
